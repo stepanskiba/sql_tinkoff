052c336e6269201e1d99080826ff63228ae29b32
stepanskiba <110840782+stepanskiba@users.noreply.github.com>
2022-12-14 00:36:50 +0500

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,3 +1,5 @@
+--1. Напишите запрос, который выведет пилотов, которые в качестве второго пилота в
+--августе этого года трижды ездили в аэропорт Шереметьево. 
 SELECT pilot.pilot_id, pilot.name
 FROM pilot JOIN flight
 ON pilot.pilot_id = flight.second_pilot_id
@@ -5,13 +7,16 @@ WHERE (flight.flight_dt BETWEEN '2022-08-01' AND '2022-08-31') AND flight.destin
 GROUP BY pilot_id
 HAVING COUNT(pilot_id) = 3;
 
+--2.  Выведите пилотов старше 45 лет, совершали полеты на самолетах с количеством
+--пассажиров больше 30. 
 SELECT pilot.pilot_id, pilot.name
 FROM pilot JOIN flight
 ON pilot.pilot_id = flight.first_pilot_id OR pilot.pilot_id = flight.second_pilot_id
 JOIN plane on flight.plane_id = plane.plane_id
 WHERE pilot.age > 45 AND plane.cargo_flg = 0 AND flight.quantity > 30;
 
-
+--3. Выведите ТОП 10 пилотов-капитанов (first_pilot_id), которые совершили наибольшее
+--число грузовых перелетов в этом году.
 SELECT pilot.pilot_id, pilot.name, COUNT(flight.flight_id)
 FROM pilot JOIN flight ON pilot.pilot_id = flight.first_pilot_id
 JOIN plane ON flight.plane_id = plane.plane_id
