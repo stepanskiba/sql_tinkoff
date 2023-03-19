1f9f49c72e8df1465502a6ee94da186446ce6465
stepanskiba <110840782+stepanskiba@users.noreply.github.com>
2023-03-20 03:23:08 +0500

    Update sql-summer.sql

--- sql-summer.sql
+++ sql-summer.sql
@@ -1,15 +1,15 @@
--- Напишите запрос, с помощью которого можно найти дубли в поле email из таблицы Sfaff.
+-- 1.Напишите запрос, с помощью которого можно найти дубли в поле email из таблицы Sfaff.
 
 SELECT email, COUNT(*) AS count
 FROM Staff
 GROUP BY email
 HAVING COUNT(*) > 1;
 
--- Напишите запрос, с помощью которого можно определить возраст каждого сотрудника из таблицы Staff на момент запроса.
+-- 2.Напишите запрос, с помощью которого можно определить возраст каждого сотрудника из таблицы Staff на момент запроса.
 SELECT staff_id, name, EXTRACT(YEAR FROM age(CURRENT_DATE, birthday)) AS Age
 FROM Staff;
   
--- Напишите запрос, с помощью которого можно определить должность (Jobtitles.name) со вторым по величине уровнем зарплаты.
+-- 3.Напишите запрос, с помощью которого можно определить должность (Jobtitles.name) со вторым по величине уровнем зарплаты.
 SELECT jobtitle 
 FROM Staff
 JOIN Jobtitles ON Staff.jobtitle_id = Jobtitles.jobtitle_id 
